a85b43c2ae73d434e23c593bdaffde8fdd20a0e0
Maflend <maks233online@gmail.com>
2022-06-11 14:37:50 +0300

    Добавил модули для клиента, админа. Настроил навигационный банер для ролей. Изменил структуру проекта клиента

---
+++ Diplom.Client.Infrastructure/Models/Enums/RoleEnum.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Diplom.Client.Infrastructure.Models.Enums
+{
+    /// <summary>
+    /// Енам ролей пользователя.
+    /// </summary>
+    public enum RoleEnum
+    {
+        /// <summary>
+        /// Администратор.
+        /// </summary>
+        [Description("Администратор")]
+        Administrator = 0,
+
+        /// <summary>
+        /// Клиент.
+        /// </summary>
+        [Description("Клиент")]
+        Client = 1
+    }
+}
